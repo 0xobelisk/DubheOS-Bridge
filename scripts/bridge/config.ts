da17222ea1a5895f162dfe258505d5fca36aea7c
henryliu <17600047539@163.com>
2025-03-17 01:11:33 +0800

    first commit

---
+++ scripts/bridge/config.ts
@@ -0,0 +1,5 @@
+type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
+
+export const NETWORK: NetworkType = 'testnet';
+export const PACKAGE_ID = '0x80351f2a98ca797204ec422662bf09c0f2fbf67d3dd92a1a426676f86e26ec95'
+export const SCHEMA_ID = '0x684c20291223a0b2070a22cdbd4f203d6d6ef66be525924d48c27a3d56107d8a'
